f42a9d423feaba7cd4964b720b74add05f1aae40
leninner <mazabandalenin180@gmail.com>
2021-09-01 20:58:10 -0500

    Update y config de webpack

---
+++ src/Components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//Componente stateless, siempre importar react
+const HelloWorld = () => {
+  return <div>Hola Mundo, esto funciona excelente</div>;
+};
+
+export { HelloWorld };
