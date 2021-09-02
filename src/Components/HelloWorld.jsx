0a3d5562191eedf87705649fe8dc7ab1f14d6687
leninner <mazabandalenin180@gmail.com>
2021-09-02 18:08:15 -0500

    Redo

--- src/Components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';
-
-//Componente stateless, siempre importar react
-const HelloWorld = () => {
-  return <div>Hola Mundo, esto funciona excelente, me gusta full</div>;
-};
-
-export { HelloWorld };
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HelloWorld } from './Components/HelloWorld';
-
-ReactDOM.render(<HelloWorld />, document.getElementById('main'));
