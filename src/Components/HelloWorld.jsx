d6e5a1a42c0f012549b05aab12df9cbdcd421856
leninner <mazabandalenin180@gmail.com>
2021-09-02 14:54:48 -0500

    Añado estilos con sass

--- src/Components/HelloWorld.jsx
+++ src/Components/HelloWorld.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/styles/App.scss';
 
 //Componente stateless, siempre importar react
 const HelloWorld = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,12 +26,19 @@ module.exports = {
           loader: 'html-loader',
         },
       },
+      {
+        test: /\.(s*)css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader'],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
     }),
   ],
 };
