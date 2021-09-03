a67a396600a40b618556ae0a4118ccb8616ddf50
leninner <mazabandalenin180@gmail.com>
2021-09-03 13:09:35 -0500

    Añado react hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { Search } from '../components/Search';
 import { Categorias } from '../components/Categorias';
@@ -8,6 +8,16 @@ import { Footer } from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
   return (
     <div className='App'>
       <Header />
